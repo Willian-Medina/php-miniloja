c0ffe3f0e2a6a87929d918ee22135140d12841f0
Willian Medina <willian-pablomedina@hotmail.com>
2023-10-18 22:59:16 -0300

    terminado html

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefa</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h1>Gerenciador de Tarefas</h1>
+    </div>
+    <div class="form">
+        <label for="task_name"></label>
+        <form action="" method="get">
+            <input type="text" name="task_name" id="" placeholder="coloque o nome aqui">
+        </form>
+        <button type="submit">Cadastrar</button>
+    </div>
+    <div class="separator">
+
+    </div>
+    <div class="list">
+        <ul>
+            <li>Tarefa 1</li>
+            <li>Tarefa 2</li>
+            <li>Tarefa 3</li>
+            <li>Tarefa 4</li>
+        </ul>
+        
+    </div>
+    <div class="footer">
+        <p></p>
+
+    </div>
+</div>
+    
+</body>
+</html>
